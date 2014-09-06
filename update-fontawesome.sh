6600907a47e6498e63aef25b705ea16be7bc59e2
Alsan Wong <awxhuang@fsll.cn>
2014-09-07 07:37:58 +0800

    update fontawesome update script

--- update-fontawesome.sh
+++ update-fontawesome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mv icons.fa.css icons.fa.css.bak
-python font-update.py icons.fa.css.bak ../Font-Awesome/fonts fontawesome-webfont.woff fontawesome-webfont.ttf > icons.fa.css
+python font-update.py icons.fa.css ../Font-Awesome/fonts fontawesome-webfont.woff fontawesome-webfont.ttf > t.css
+mv t.css icons.fa.css
